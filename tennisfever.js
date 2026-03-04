9530722f505ff34af663a29dccc36cb01ce97606
23235aa <23235aa@hvhs.school.nz>
2026-03-04 19:46:46 +1300

    fixed shooting mechanic 2

--- tennisfever.js
+++ tennisfever.js
@@ -1,34 +1,33 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
+
 function setup() {
-console.log("setup: ");
-canvasWidth = 500;
-canvasHeight = 500;
-cnv= new Canvas(canvasWidth,canvasHeight);
-wallLH  = new Sprite(0, 500/2, 8, 500, 'k');
-wallLH.color = 'black';
-wallRH  = new Sprite(500, 500/2, 5, 500, 'k');
-wallRH.color = 'blue';
-wallBot = new Sprite(250,500,500,50,'k');
-wallBot.color = 'red';
-wallTop = new Sprite(250, 10, 500, 50, 'k');
-player_1 = new Sprite(500/2, 500/2, 50, 'd');
-player_1.color = 'blue';
-player_1.vel.x = 5;
-player_1.bounciness = 5;
-player_1.friction = 20;
-player_1.drag = 1;	
-tennisBalls = new Group();
+	console.log("setup: ");
+	canvasWidth = 500;
+	canvasHeight = 500;
+	cnv= new Canvas(canvasWidth, canvasHeight);
+	wallLH  = new Sprite(0, 500/2, 8, 500, 'k');
+	wallLH.color = 'black';
+	wallRH  = new Sprite(500, 500/2, 5, 500, 'k');
+	wallRH.color = 'blue';
+	wallBot = new Sprite(250,500,500,50,'k');
+	wallBot.color = 'red';
+	wallTop = new Sprite(250, 10, 500, 50, 'k');
+	player_1 = new Sprite(500/2, 500/2, 50, 'd');
+	player_1.color = 'blue';
+	player_1.vel.x = 5;
+	player_1.bounciness = 5;
+	player_1.friction = 20;
+	player_1.drag = 1;	
+	tennisBalls = new Group();
 }
 function draw() {
 	background('white')
 }
 function mousePressed(){
-	if (mouse.presses()) {
-shootTennisBalls();
-	}
-	};
+	shootTennisBalls();
+};
 function shootTennisBalls(){
 	balls = new Sprite(player_1.x, player_1.y,10);
 	balls.color = 'red'
@@ -38,6 +37,7 @@ function shootTennisBalls(){
 	//Added this feature so that the tennis balls don't collide with the player and go in weird directions.
 	//Doing this the tennis balls overlap the player which means they can't collide. 
 	balls.overlap(player_1);
+	//added in groups to handle future colloisions with targets. 
 	tennisBalls.add(balls);
 	balls.life = 60;
 }
