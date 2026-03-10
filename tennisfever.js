79eda50fa487b6f4961f15ac27494ec198c0c914
23235aa <23235aa@hvhs.school.nz>
2026-03-10 15:14:31 +1300

    make the targets respawn when hit

--- tennisfever.js
+++ tennisfever.js
@@ -1,6 +1,11 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
+function preload() {
+
+  imgTennisplayer = loadImage('assets/images/tennisplayer.png');
+  imgTarget = loadImage('assets/images/tennistarget.png');
+}
 function setup() {
 	console.log("setup: ");
 	canvasWidth = 500;
@@ -18,22 +23,30 @@ function setup() {
 	player_1.bounciness = 5;
 	player_1.friction = 20;
 	player_1.drag = 1;	
+	player_1.image = (imgTennisplayer);
+	imgTennisplayer.resize(50,50);
 	tennisBalls = new Group();
 	targetGroup = new Group();
 	for (i = 0; i < 5; i++) {
-		targetSpawnX = random(0,500)//making sure that the target don't sapwn at the same place and are clumped together
+		targetSpawnX = random(0,500)//making sure that the target don't spawn at the same place and are clumped together
 		targetSpawnY = random(0,500)//making sure that the target don't sapwn at the same place and are clumped together
   target = new Sprite(targetSpawnX,targetSpawnY,60,60,'d')
 target.friction = 0.5;
-target.speed = 2.9;
-targetGroup.add(target);
+target.speed = 1.3;
+targetGroup.add(target);//Make sure that every target moves towards the player
 }
+target.image = (imgTarget)
+imgTarget.resize(50,50)
 }
+
 function draw() {
 	background('white')
+	player_1.rotation = 0;
 	if (mouse.presses()) {
 shootTennisBalls();
 };
+
+
 if (kb.pressing('left')) {
 
     // Set sprite's velocity to the left
@@ -68,9 +81,19 @@ if (kb.released('up')){
 else if (kb.released('down')){
 	player_1.vel.y = 0
 };
-targetGroup.moveTowards(player_1,0.005)//The targets move towards the player
+targetGroup.moveTowards(player_1,0.05)//The targets move towards the player
+targetGroup.collides(tennisBalls, func2Call);
+function func2Call(_target, _tennisBalls){
+	_target.remove();
+	_tennisBalls.remove();
+	target2SpawnX = random(0,500);
+	target2SpawnY = random(0,500);
+	target2 = new Sprite(target2SpawnX,targetSpawnY,60,60,'d')
+	targetGroup.add(target2);
+target2.image = (imgTarget)
+imgTarget.resize(50,50);
 }
-
+}
 function shootTennisBalls(){
 	balls = new Sprite(player_1.x, player_1.y,10);
 	balls.color = 'red'
