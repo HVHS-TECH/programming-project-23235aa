4765644252e01608f085b36d19d1b376e4c66235
23235aa <23235aa@hvhs.school.nz>
2026-03-13 12:26:08 +1300

    fixed spawn issue and start6ed timer

--- tennisfever.js
+++ tennisfever.js
@@ -1,6 +1,8 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
+let timeLimit = 10;
+let countDown;
 function preload() {
 
 	imgTennisplayer = loadImage('assets/images/tennisplayer.png');
@@ -28,8 +30,17 @@ function setup() {
 	tennisBalls = new Group();
 	targetGroup = new Group();
 	for (i = 0; i < 5; i++) {
-		targetSpawnX = random(0, 500)//making sure that the target don't spawn at the same place and are clumped together
-		targetSpawnY = random(0, 500)//making sure that the target don't sapwn at the same place and are clumped together
+		let targetSpawnX, targetSpawnY;
+		let minDistanceFromPlayer = 150;  //The minimum distance I want between the player and the target so they don't collide
+		do {
+			targetSpawnX = random(0, 500)//making sure that the target don't spawn at the same place and are clumped together.
+			//Also it keeps on choosing a new value of x until if the disatnce between target
+			//  and the player is lower than the previous defined one
+			targetSpawnY = random(0, 500)//making sure that the target don't sapwn at the same place and are clumped together
+			//Also it keeps on choosing a new value of y until if the disatnce between target
+			//  and the player is lower than the previous defined one
+		} while (dist(targetSpawnX, targetSpawnY, player_1.x, player_1.y) < minDistanceFromPlayer); //If the distance is lower it keeps on telling 
+		//the computer to do the do loop
 		target = new Sprite(targetSpawnX, targetSpawnY, 60, 60, 'd')
 		target.friction = 0.5;
 		target.speed = 1.3;
@@ -92,10 +103,19 @@ function draw() {
 		targetGroup.add(target2);
 		target2.image = (imgTarget)
 		imgTarget.resize(50, 50);
-	}
-	if(player_1.collides(targetGroup)){
+	}//Called the remove fucntion inside the draw loop becuase I want a new Target to spawn in and I need a loop for that
+	//Also used the groups to call them as they conrtol everything about the sprite. Could not have used the sprite becuase it is
+	//only called in a function. 
+	if (player_1.collides(targetGroup)) {
 		print("Game Over")
 		noLoop();
+	}
+	let currentTime = int(millis()/1000)
+	countDown = timeLimit - currentTime;
+	if (countDown < 0){
+		countDown = 0;
+		textSize(32);
+		text("Game/")
 	}
 }
 function shootTennisBalls() {
